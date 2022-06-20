d61a6f9d9cae742d5ba11e341eac2aa7bbdceb90
thejaviertc <javier.toribiocouz@gmail.com>
2022-06-21 00:54:01 +0200

    Commit Inicial

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/infima/dist/css/default/default.css'),
+  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/javiertc/personal/urbanlife/militaryrp-docs/src/css/custom.css'),
+];
