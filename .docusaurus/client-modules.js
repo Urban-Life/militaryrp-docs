bfdcc8cef13cd7112bf30fa433c99b6d007e5dd4
thejaviertc <javier.toribiocouz@gmail.com>
2022-06-21 18:33:01 +0200

    Eliminado Typescript

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/home/javiertc/personal/urbanlife/militaryrp-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/javiertc/personal/urbanlife/militaryrp-docs/src/css/custom.css'),
-];
